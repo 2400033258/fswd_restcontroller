02dc5be3431620b93f6d7d4a8127dc2e4a0c4734
ParnikaChalla <challaparnika09@gmail.com>
2026-03-05 17:13:27 +0530

    first commit

---
+++ src/main/java/com/klu/AppController.java
@@ -0,0 +1,25 @@
+package com.klu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+	@GetMapping("demo")
+	
+   public int fun1() {
+	return 10;
+	}
+	@GetMapping("demo2")
+	public String fun2() {
+	return "Welcome to KL University";
+	}
+	@GetMapping("/demo3/{a}/{b}")
+public int fun3(@PathVariable("a") int a, @PathVariable("b") int b) {
+		return a+b;
+		
+	
+}
+}
+
